4576a2afefcfd2b21799fda1e26d7b863044a47c
fmoraisrj <felipejpa15@gmail.com>
2013-09-16 10:34:32 -0300

    Temrinei o teste do modelo de Task

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -6,16 +6,25 @@ describe Task do
     List.new({ id: "1", name_list: "my_new_list", item1: "my_item1", item2: "my_item2", item3: "my_item3" })
   end
 
+  let :wrong_list do
+    List.new({ id: nil, name_list: "my_new_list", item1: "my_item1", item2: "my_item2", item3: "my_item3" })
+  end
+
   it "is valid" do
-    list = List.stub(:find).and_return(new_list) 
+    
     task = Task.new
-    task.list_id = list.id
+    task.list = new_list
     task.status = true
 
     task.should be_valid    
   end
 
-  xit "is not valid" do
+  it "is not valid" do
     
+    task = Task.new
+    # task.list = wrong_list
+    task.status = true
+
+    task.should_not be_valid    
   end
 end
