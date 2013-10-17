1f503cdf8ee45c41ad86a6a9a758dad782a27ec2
fmoraisrj <felipejpa15@gmail.com>
2013-10-17 15:37:59 -0300

    criando a routa da nova home; criando o home#index; criados os forms de edit e new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TodoList::Application.routes.draw do
+  
   get "home/index"
 
   devise_for :users
@@ -23,7 +24,7 @@ TodoList::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root to: 'lists#index'
+  root to: 'home#index'
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
